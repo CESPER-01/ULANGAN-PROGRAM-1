62522bc39f92dabcca31188c682ff94362f8a8bb
Sam_76 <muhammadusamah76@gmail.com>
2025-12-10 21:25:44 +0800

    final

---
+++ Soal-1.js
@@ -0,0 +1,3 @@
+//Buatlah 3 buah variable dengan tipe data yang berbeda dan jelaskan tipe data tersebut menggunakan comment
+
+//Tampilkan ketiga variable pada console
---
+++ Soal-2.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Untuk yang ini jangan pakai AI
+ * 
+ */
+
+const gender = "M"
+
+if (gender == "M") {
+    gender = "Laki-Laki"
+}else if (gender == "F") {
+    gender = "Perempuan"
+} else {
+    gender = "Tidak di temukan"
+}
+
+console.log(gender);
+
+
+//Jawblah menggunakan comment kenapa kode diatas tidak mengeluarkan output yang diharapkan
+
